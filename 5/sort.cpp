8150e689b622b214bfc51e5eff1acda373cf65eb
Nikolay Kotlyarov <nikolay.kotlyarov1@gmail.com>
2022-06-20 17:32:27 +0600

    baseline

---
+++ 5/sort.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 1 on 20.06.2022.
+//
+
+#include <iostream>
+#include <cstdlib>
+
+
---
+++ 5/sort.h
@@ -0,0 +1,8 @@
+//
+// Created by 1 on 20.06.2022.
+//
+
+#ifndef ALGORITHMS_SORT_H
+#define ALGORITHMS_SORT_H
+
+#endif //ALGORITHMS_SORT_H
