c95565efc3f6dc79f6fd2c56e4b08aa08b54dcdc
Nikolay Kotlyarov <nikolay.kotlyarov1@gmail.com>
2022-06-24 13:27:59 +0600

    feat: hash implementation

--- 8/hash.cpp
+++ 8/hash.cpp
@@ -2,4 +2,29 @@
 // Created by 1 on 24.06.2022.
 //
 
+#include <iostream>
+#include <unordered_map>
 #include "hash.h"
+
+using namespace std;
+
+void present_hash(){
+    unordered_map<string, int> umap;
+    string key = "PI";
+    // inserting values by using [] operator
+    umap["GeeksforGeeks"] = 10;
+    umap["Practice"] = 20;
+    umap["Contribute"] = 30;
+
+    umap.insert(make_pair("e", 2.718));
+
+    key = "lambda";
+    if (umap.find(key) == umap.end())
+        cout << key << " not found\n";
+    else
+        cout << "Found " << key << endl;
+
+    // Traversing an unordered map
+    for (auto x : umap)
+        cout << x.first << " " << x.second << endl;
+}
--- 8/hash.h
+++ 8/hash.h
@@ -4,5 +4,5 @@
 
 #ifndef ALGORITHMS_HASH_H
 #define ALGORITHMS_HASH_H
-
+void present_hash();
 #endif //ALGORITHMS_HASH_H
