87457c261dcb4be8bca4465a8261d682db458ca2
Nikolay Kotlyarov <nikolay.kotlyarov1@gmail.com>
2022-06-24 11:06:28 +0600

    feat: hash baseline

---
+++ 8/hash.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 1 on 24.06.2022.
+//
+
+#include "hash.h"
---
+++ 8/hash.h
@@ -0,0 +1,8 @@
+//
+// Created by 1 on 24.06.2022.
+//
+
+#ifndef ALGORITHMS_HASH_H
+#define ALGORITHMS_HASH_H
+
+#endif //ALGORITHMS_HASH_H
